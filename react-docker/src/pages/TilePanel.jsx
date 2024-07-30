aa8e5762fb2cb8583a9d875dc38f6279362570bb
Sacha Duperret <duperretsacha@gmail.com>
2024-07-30 22:03:38 +0000

    rm save

--- react-docker/src/pages/TilePanel.jsx
+++ react-docker/src/pages/TilePanel.jsx
@@ -15,16 +15,16 @@ import benevolesIcon from './pictures/benevoles.png';
 const TilePanel = () => {
   const appLinks = [
     
-    { name: 'Discord', link: 'https://discord.com/channels/1265099843978793080/1265099844444487802', icon: 'app2-icon', image: discordIcon },
-    { name: 'Drive', link: 'https://drive.google.com/drive/folders/167DuBF_YD8rtS-3PGuUm6NcPyNovjZPu', icon: 'app2-icon', image: driveIcon },
-    { name: 'Mail', link: 'https://mail.google.com/', icon: 'app2-icon', image: mailsIcon },
-    { name: 'OpenProject', link: 'https://op.assomayday.org', icon: 'app1-icon', image: opIcon },
-    { name: 'Strapi', link: 'https://strapi.assomayday.org', icon: 'app2-icon', image: strapiIcon },
-    { name: 'Dolibarr', link: 'https://dolibarr.assomayday.org', icon: 'app2-icon', image: dolibarrIcon },
+    { name: 'Discord - Echanges interne', link: 'https://discord.com/channels/1265099843978793080/1265099844444487802', icon: 'app2-icon', image: discordIcon },
+    { name: 'Drive - Dossier Mayday 2025', link: 'https://drive.google.com/drive/folders/167DuBF_YD8rtS-3PGuUm6NcPyNovjZPu', icon: 'app2-icon', image: driveIcon },
+    { name: 'Mail - Echanges externes', link: 'https://mail.google.com/', icon: 'app2-icon', image: mailsIcon },
+    { name: 'OpenProject - Gestion de projet', link: 'https://op.assomayday.org', icon: 'app1-icon', image: opIcon },
+    { name: 'Strapi - Gestion site web', link: 'https://strapi.assomayday.org', icon: 'app2-icon', image: strapiIcon },
+    { name: 'Dolibarr - Gestion financière', link: 'https://dolibarr.assomayday.org', icon: 'app2-icon', image: dolibarrIcon },
     { name: 'Site officiel', link: 'https://maydayfestival.fr', icon: 'app1-icon', image: Logo },
     { name: 'Site bénévoles', link: 'https://benevoles.maydayfestival.fr', icon: 'app1-icon', image: benevolesIcon },
-    { name: 'Trompette', link: 'https://trompette.espace120.fr/ui/login/', icon: 'app2-icon', image: trompetteIcon },
-    { name: 'Traefik', link: 'https://monitor.assomayday.org', icon: 'app2-icon', image: traefikIcon },
+    { name: 'Trompette - Gestion d\'identité', link: 'https://trompette.espace120.fr/ui/login/', icon: 'app2-icon', image: trompetteIcon },
+    { name: 'Traefik - Proxy', link: 'https://monitor.assomayday.org', icon: 'app2-icon', image: traefikIcon },
   ];
 
   return (
